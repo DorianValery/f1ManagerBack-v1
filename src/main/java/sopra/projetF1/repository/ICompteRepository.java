50cf08006abbb88c0c087bb6acd5ed2387832359
Fabiola <fabiola.ouedraogo@gmail.com>
2021-11-14 14:56:41 +0100

    jpql repository Compte

--- src/main/java/sopra/projetF1/repository/ICompteRepository.java
+++ src/main/java/sopra/projetF1/repository/ICompteRepository.java
@@ -1,12 +1,26 @@
 package sopra.projetF1.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sopra.projetF1.model.Compte;
 
-public interface ICompteRepository extends JpaRepository<Compte, Long> {
+public interface ICompteRepository extends JpaRepository<Compte, Long> {	
 //findAllCompte
+@Query("select c from Compte c")
+List<Compte> findAllCompte(@Param("compt") Compte compte);
+
 //findCompteById
-//findAllCompte
+@Query("select c from Compte c where c.id = :id")
+Optional<Compte> findFormateurById(@Param("id") Long id);
+
 //findCompteByType
+@Query("select c from Compte c where c.type = :type")
+Optional<Compte> findFormateurByType(@Param("type") Long type);
+
 }
